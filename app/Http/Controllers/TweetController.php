f604065c460ac4eaada010d41760aef8ae55471a
dausuke <ksye616@gmail.com>
2023-01-13 02:25:14 +0900

    画像投稿作成

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Image;
-use Storage;
+use Illuminate\Support\Facades\Storage;
 
 use App\Models\Tweet;
 
@@ -14,7 +14,7 @@ class TweetController extends Controller
     function index()
     {
         try {
-            $response = Tweet::latest()->with('user')->get();
+            $response = Tweet::latest()->with('user', 'images')->get();
             return response()->json($response, 200);
         } catch (\Exception $e) {
             Logger($e);
@@ -25,13 +25,13 @@ class TweetController extends Controller
 
     function create(Request $request)
     {
+        DB::beginTransaction();
         try {
             $request->validate(
                 [
                     'comment_to' => 'nullable|numeric',
                     'body' => 'nullable|string',
-                    'images' => 'nullable|array:image',
-                    'images.*.image' => 'string'
+                    'images' => 'nullable|array',
                 ]
             );
 
@@ -41,21 +41,22 @@ class TweetController extends Controller
             $tweet = $user->tweets()->create($data);
 
             if ($request->has('images')) {
-                $path = 'tweets/images';
-                $request_images = $request->only('images');
+                $path = 'public/tweets/images';
 
-                foreach ($request_images as $request_image) {
-                    $image = Image::make($request_image)->orientate()->encode('jpg');
+                foreach ($request->images as $request_image) {
+                    $image = Image::make($request_image)->orientate()->encode('png');
                     $hash = md5($image->__toString());
-                    $file_name = "{$hash}.jpg";
-                    Storage::makeDirectory($path);
+                    $file_name = "{$hash}.png";
                     Storage::put($path . '/' . $file_name, $image->encode(), 'public');
 
                     $image->destroy();
                     $tweet->images()->create(['image' => $file_name]);
                 }
             }
+
+            DB::commit();
         } catch (\Exception $e) {
+            DB::rollBack();
             Logger($e);
             abort(404);
         }
